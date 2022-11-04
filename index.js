af26cc90e7192f3a0631aa4482ffcc7eceb945e4
Danielle <dsg1457.dg@gmail.com>
2022-11-04 15:07:08 -0600

    new items

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const express =require('express');
+//const { readSync } = require('fs');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+
+app.set('view engine', 'handlebars');
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//home page route
+app.get('/', (req, res) => 
+    res.render('index', {
+    title: 'Member App',
+    members
+})
+);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+//app.get('/', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,  => console.log('Server started on port ${PORT}'));
+app.listen(PORT, ()  => console.log(`Server started on port ${PORT}`));
+
